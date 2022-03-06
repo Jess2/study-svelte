61401f6ae5667d265d21fefb6c0147f99553797b
JESS2 <jess2.developer@gmail.com>
2022-03-06 21:41:51 +0900

    선언적 렌더링 학습

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+  let name = 'World';
+  let age = 99;
+
+  function assign() {
+    name = 'Jess2';
+    age = 31;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Hello {name}</h1>
+<h2 class={age < 85 ? 'active' : ''}>my age : {age}</h2>
+<img src="" alt={name} />
+<input type="text" bind:value={name} />
+<button on:click={assign}>
+  클릭!
+</button>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: red;
+  }
+  .active {
+    color: blue;
+  }
 </style>
