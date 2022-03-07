9eab1e74e7d63901a753186f3c18d25ed35ac565
JESS2 <jess2.developer@gmail.com>
2022-03-08 00:31:50 +0900

    반복문 학습

---
+++ src/Loop.svelte
@@ -0,0 +1,32 @@
+<script>
+  let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
+
+</script>
+
+<h2>Fruits</h2>
+<ul>
+  {#each fruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<h2>Fruits Reverse</h2>
+<ul>
+  {#each [...fruits].reverse() as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<h2>Fruits Slice - 2</h2>
+<ul>
+  {#each fruits.slice(-2) as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<h2>Fruits</h2>
+<ul>
+  {#each fruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
