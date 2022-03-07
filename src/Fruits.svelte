110891eb0156d9070fa173480abfb02d6649e11f
JESS2 <jess2.developer@gmail.com>
2022-03-08 00:51:27 +0900

    컴포넌트와 props 학습

---
+++ src/Fruits.svelte
@@ -0,0 +1,26 @@
+<script>
+  // Props
+  export let fruits;
+  export let reverse;
+  export let slice;
+
+  let computedFruits = [];
+  let name = '';
+
+  if (reverse) {
+    computedFruits = [...fruits].reverse()
+    name = 'Reverse';
+  } else if (slice) {
+    computedFruits = fruits.slice(...slice.split(','));
+    name = `slice ${slice}`;
+  } else {
+    computedFruits = fruits;
+  }
+</script>
+
+<h2>Fruits {name}</h2>
+<ul>
+  {#each computedFruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
