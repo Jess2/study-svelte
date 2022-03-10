61865cdbf09ba44d2ffabdc007044a316451d266
JESS2 <jess2.developer@gmail.com>
2022-03-10 21:09:26 +0900

    라이프 사이클 학습

---
+++ src/lifeCycle/Child.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+
+  onMount(() => {
+    console.log('onMount - 컴포넌트 연결 이후에 실행됨.');
+    const h1 = document.querySelector('h1');
+    console.log(h1.innerText);
+
+    return () => {
+      console.log('destroy in mount');
+    };
+  });
+
+  onDestroy(() => {
+    console.log('onDestroy - 컴포넌트 연결 해제 이전에 실행됨.')
+    const h1 = document.querySelector('h1');
+    console.log(h1.innerText);
+  });
+</script>
+
+<h1>Hello!</h1>
---
+++ src/lifeCycle/Parent.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Child from './Child.svelte';
+
+  let toggle = false;
+</script>
+
+<button on:click={() => {toggle = !toggle}}>
+  toggle
+</button>
+
+{#if toggle}
+  <Child />
+{/if}
