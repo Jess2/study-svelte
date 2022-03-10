4606b1ae45da90de098677d95088232e14326545
JESS2 <jess2.developer@gmail.com>
2022-03-10 21:47:20 +0900

    라이프 사이클 - beforeUpdate, afterUpdate 학습

--- src/lifeCycle/Child.svelte
+++ src/lifeCycle/Child.svelte
@@ -1,14 +1,30 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy, beforeUpdate, afterUpdate } from 'svelte';
+
+  let name = 'something';
+  let h1;
+
+  function moreDot() {
+    name += '.';
+  }
+
+  beforeUpdate(() => {
+    console.log('Before update');
+    console.log(h1 && h1.innerText);
+  });
 
   onMount(() => {
     console.log('onMount - 컴포넌트 연결 이후에 실행됨.');
-    const h1 = document.querySelector('h1');
-    console.log(h1.innerText);
+    h1 = document.querySelector('h1');
 
     return () => {
       console.log('destroy in mount');
     };
+  });
+
+  afterUpdate(() => {
+    console.log('After update');
+    console.log(h1.innerText);
   });
 
   onDestroy(() => {
@@ -18,4 +34,4 @@
   });
 </script>
 
-<h1>Hello!</h1>
+<h1 on:click={moreDot}>{name}</h1>
