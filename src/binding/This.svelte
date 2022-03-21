cd1a27a90c6688926c009bffded93b80ec857435
JESS2 <jess2.developer@gmail.com>
2022-03-21 22:36:34 +0900

    일반 요소 바인딩(this) 학습

---
+++ src/binding/This.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { tick } from 'svelte';
+
+  let isShow = false;
+  let inputEl;
+
+  async function toggle() {
+    isShow = !isShow;
+    await tick();
+    // const inputEl = document.querySelector('input');
+    inputEl && inputEl.focus();
+  }
+</script>
+
+<button on:click={toggle}>Edit!</button>
+
+{#if isShow}
+  <input bind:this={inputEl} />
+{/if}
