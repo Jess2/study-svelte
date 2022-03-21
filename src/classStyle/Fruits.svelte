8d1b4cefe3a71698c6071221ea6e8aeef5a90926
JESS2 <jess2.developer@gmail.com>
2022-03-21 21:51:24 +0900

    스타일 유효범위(Hash)와 전역화(Global) 학습

---
+++ src/classStyle/Fruits.svelte
@@ -0,0 +1,6 @@
+<h2>Fruits</h2>
+<ul class="fruits">
+  <li>Apple</li>
+  <li>Banana</li>
+  <li>Cherry</li>
+</ul>
---
+++ src/classStyle/FruitsParents.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Fruits from './Fruits.svelte';
+</script>
+
+<h2>App</h2>
+<ul class="fruits">
+  <li>Apple</li>
+  <li>Banana</li>
+  <li>Cherry</li>
+</ul>
+
+<Fruits />
+
+<style>
+  :global(.fruits) {
+    color: red;
+  }
+
+  .fruits {
+    background-color: yellow;
+  }
+</style>
