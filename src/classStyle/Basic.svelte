15923b7e1e6386c1673c4166b929b543c1dffc10
JESS2 <jess2.developer@gmail.com>
2022-03-21 21:21:08 +0900

    클래스와 스타일 속성 바인딩

---
+++ src/classStyle/Basic.svelte
@@ -0,0 +1,49 @@
+<script>
+  let isActive = false;
+  let color = {
+    t: 'tomato',
+    w: '#fff'
+  }
+  let letterSpacing = 'letter-spacing: 10px;';
+
+  function toggle() {
+    isActive = !isActive;
+  }
+</script>
+
+<button on:click={toggle}>
+  Toggle!
+</button>
+
+<div class={isActive ? 'active' : ''}>
+  Box1
+</div>
+
+<div class:active={isActive}>
+  Box2
+</div>
+
+<h2 style="background-color: {color.t}; color:{color.w}; {letterSpacing}">
+  Hello world!
+</h2>
+
+<style>
+  div {
+    width: 120px;
+    height: 200px;
+    background-color: rosybrown;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    color: white;
+    font-size: 20px;
+    transition: .4s;
+    margin: 10px;
+  }
+
+  .active {
+    width: 250px;
+    background-color: forestgreen;
+  }
+</style>
